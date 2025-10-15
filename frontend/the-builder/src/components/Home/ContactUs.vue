ce5fab9917dc00015139ce672324396ad6d213aa
xBlackBunnyx <loremd99@outlook.com>
2025-10-15 18:49:04 +0100

    contact-us updated

--- frontend/the-builder/src/components/Home/ContactUs.vue
+++ frontend/the-builder/src/components/Home/ContactUs.vue
@@ -1,6 +1,6 @@
 <template>
     <!-- We can add here all the Linkedin and GitHub information -->
-  <v-footer> 
+  <v-footer > 
     <div style="height: 100%; justify-content: right; "
     class="text-secondary">
         <h3 class="single-line">  Contact us!  </h3>
@@ -10,13 +10,21 @@
       <!-- también que el texto del centro no se corte fijando el tamaño y poner los enlaces-->
 
     <div>
-     <v-container class="pt-15">
+     <v-container class="d-flex justify-center">
       <v-row
+        class="flex-nowrap"
+        style="max-width: 1200px; width: 100%; flex-wrap: nowrap;"
         align="center"
+        no-gutters
+        justify="space-between"
+
       >
-        <v-col>
+        <v-col 
+        cols="auto"
+        class="d-flex justify-center">
           <v-sheet>
             <v-img
+            cover
             :width="200"
               src = "../../assets/bard-behind-no-bg.png"
               >
@@ -24,36 +32,52 @@
           </v-sheet>
         </v-col>
 
-         <v-col>
-          <v-sheet class="single-line">
-            <v-text-field variant="plain">
-              <v-icon icon="fa-github" class="text-secondary" size="small"></v-icon>
-              <p class=""> The builder </p>
-            </v-text-field>
-            <v-text-field variant="plain">
-              <v-icon icon="fa-linkedin" class="text-secondary" size="small"></v-icon>
+         <v-col 
+          cols="4"
+         class="d-flex flex-column justify-center align-center text-center">
+          <v-sheet color="transparent" elevation="0">
+            <v-row class="align-center justify-center" no-gutters>
+                <v-col class="d-flex align-center"> 
+              <v-icon icon="fa-github" class="text-secondary me-2" size="small"></v-icon>
+              <p> The builder </p>
+              
+                </v-col>
+            </v-row>
+            <v-row class="align-center justify-center" no-gutters>
+                <v-col class="d-flex align-center"> 
+              <v-icon icon="fa-linkedin" class="text-secondary me-2" size="small"></v-icon>
               <p> Juan Guillermo Zafra Fernández</p>
-            </v-text-field>
-            <v-text-field variant="plain">
-              <v-icon icon="fa-linkedin" class="text-secondary" size="small"></v-icon>
+                </v-col>
+            </v-row>
+            <v-row class="align-center justify-center" no-gutters>
+                <v-col class="d-flex align-center"> 
+              <v-icon icon="fa-linkedin" class="text-secondary me-2" size="small"></v-icon>
               <p> Lorena Martín Dorta</p>
-            </v-text-field>
+                </v-col>
+            </v-row>  
+
           </v-sheet>
         </v-col>
 
-        <v-col>
+        <v-col 
+        cols="auto"
+        class="d-flex justify-center">
           <v-sheet>
             <v-img
+            cover
             :width="200"
             src = "../../assets/bard-front-no-bg.png"
             ></v-img>
           </v-sheet>
         </v-col>
 
-         <v-col>
+         <v-col 
+          cols="auto"
+         class="d-flex justify-center">
           <v-sheet>
             <v-img
-            :width="200"
+            cover
+            :width="300"
             src = "../../assets/LoL_Logo_Rendered_LARGE.png"
             ></v-img>
           </v-sheet>

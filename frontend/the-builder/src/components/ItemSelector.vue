34b1c5615701206928ac2a4716e0538f0bb8b771
xBlackBunnyx <loremd99@outlook.com>
2025-10-20 16:59:18 +0100

    Update ItemSelector.vue

--- frontend/the-builder/src/components/ItemSelector.vue
+++ frontend/the-builder/src/components/ItemSelector.vue
@@ -1,44 +1,45 @@
 <template>
-  <div class="text-center">
-    <v-menu
-      v-model="menu"
-      :close-on-content-click="false"
-
-    >
+  <div class="d-flex justify-space-around">
+    <v-menu>
       <template v-slot:activator="{ props }">
         <button
           class="selectorstyle"
           v-bind="props"
         >
-          Item
-      </button>
-      </template>
-
-      <v-card min-width="250" class="bgselector">
-        <v-card-actions>
-          <button
-            variant="text"
-            @click="menu = false"
-          >
-            Cancel
+          Activator slot
         </button>
-          <button
-            color="primary"
-            variant="text"
-            @click="menu = false"
-          >
-            Save
-    </button>
-        </v-card-actions>
-      </v-card>
+      </template>
+    <v-list 
+    :items="items"
+    @update:selected="selected"> 
+      <v-list-subheader>REPORTS</v-list-subheader>
+          <v-list-item
+           v-for="(item, i) in items"
+           :key="i"
+           :value="item"
+           active-color="primary"
+           >
+
+            <template v-slot:prepend>
+              <v-icon :icon="item.icon"></v-icon>
+            </template>
+
+            <v-list-item-title v-text="item.text"></v-list-item-title>
+          </v-list-item>
+    </v-list>
     </v-menu>
   </div>
 </template>
 
 <script>
-  import { ref } from 'vue'
+import { ref } from 'vue'
 
-  const menu = ref(false)
+  const selected = ref([])
+  const items = ref( [
+    { text: 'Real-Time', icon: 'mdi-clock' },
+    { text: 'Audience', icon: 'mdi-account' },
+    { text: 'Conversions', icon: 'mdi-flag' },
+  ])
 
 </script>
 

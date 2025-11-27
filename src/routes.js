1fe535cdfa04d53ed47bf6f9173d9116ddc1062f
xBlackBunnyx <loremd99@outlook.com>
2025-11-27 16:36:00 +0000

    Testing (again)

--- src/routes.js
+++ src/routes.js
@@ -5,10 +5,7 @@ import BuildCreator from "./components/BuildCreator/BuildCreator.vue";
 import {createRouter, createWebHistory} from "vue-router";
 
 const router = createRouter({
-    history: createWebHistory(
-        // import.meta.env.BASE_URL
-        '#'
-    ),
+    history: createWebHistory(import.meta.env.BASE_URL),
     routes:[
         {path:"/", component: Home},
         {path: "/dailybuild", component: DailyBuild, name: "DailyBuild"},

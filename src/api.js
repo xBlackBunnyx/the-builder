c6e24347934fca67cac2a9bb9db56fb9a8d2bde7
xBlackBunnyx <loremd99@outlook.com>
2025-11-27 15:00:37 +0000

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3001/api';
+const API_BASE_URL = 'https://the-builder-wx6f.onrender.com/api';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
